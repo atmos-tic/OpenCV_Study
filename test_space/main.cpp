07b83b7f90e749470639fa997d012705c1390e16
atmos-tic <hantai8118@gmail.com>
2018-02-06 14:49:08 +0900

    [add] make track bar

--- test_space/main.cpp
+++ test_space/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, const char* argv[])
 {
   // （1）DirectShowを使ってカメラキャプチャを行う。
   // また、カメラIDが0番のカメラについてキャプチャの準備を行う
-  int camera_id = 1;
+  int camera_id = 0;
   //cv::VideoCapture cap(cv::CAP_DSHOW + camera_id);
   cv::VideoCapture cap(camera_id);
 
@@ -28,14 +28,20 @@ int main(int argc, const char* argv[])
 
   cv::Mat dst, gray, img1, img2;
 
+  /*トラックバー用window作成*/
+   cv::namedWindow("track", cv::WINDOW_AUTOSIZE);
+   int thredshold_value = 80;
+   cv::createTrackbar("threshold", "track", &thredshold_value, 255);
+   //cv::imshow("track", dst);
   for (;;)
   {
     // （4）カメラから1フレーム分の画像データを取得して、変数frameに格納する
     cap >> frame;
     cv::cvtColor(frame, gray, cv::COLOR_RGB2GRAY);
     //cv::equalizeHist(gray, img1);
-    cv::GaussianBlur(gray, img2, cv::Size(7,7), 10, 10);
-    cv::Canny(img2, dst, 40, 150);
+    threshold(gray, dst, thredshold_value, 200, 0);
+    // cv::GaussianBlur(gray, img2, cv::Size(7,7), 10, 10);
+    // cv::Canny(img2, dst, 40, 150);
     // 画像データ取得に失敗したらループを抜ける
     if (frame.empty()) break;
 
