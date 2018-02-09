222aa6dd5c202785444401d3cd115a8767f8a9e4
atmos-tic <hantai8118@gmail.com>
2018-02-09 21:12:05 +0900

    [add] diff dynamics program

--- test_space/main.cpp
+++ test_space/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, const char* argv[])
   // また、カメラIDが0番のカメラについてキャプチャの準備を行う
   int camera_id = 0;
   //cv::VideoCapture cap(cv::CAP_DSHOW + camera_id);
-  cv::VideoCapture cap(camera_id);
+   cv::VideoCapture cap(camera_id);
 
   // （2）カメラIDが0番のカメラが正しくオープンできているかをチェックする（正しくオープンできていなければエラー終了する）
   if (!cap.isOpened())
@@ -21,8 +21,8 @@ int main(int argc, const char* argv[])
   cap.set(cv::CAP_PROP_FPS, 30.0);
 
   // 画像表示用のウィンドウを生成する
-  cv::namedWindow("in", cv::WINDOW_AUTOSIZE);
-  cv::namedWindow("out", cv::WINDOW_AUTOSIZE);
+   cv::namedWindow("in", cv::WINDOW_AUTOSIZE);
+   cv::namedWindow("out", cv::WINDOW_AUTOSIZE);
 
   // 画像データを格納するための変数を宣言する
   cv::Mat frame;
@@ -41,16 +41,17 @@ int main(int argc, const char* argv[])
       cv::cvtColor(frame,img,CV_RGB2GRAY);
       //cv::cvtColor(frame, gray, cv::COLOR_RGB2GRAY);
       //threshold(gray, dst, thredshold_value, 200, 0);
-      img.copyTo(pre_img[0],img)
-      img.copyTo(pre_img[1],img)
+      img.copyTo(pre_img[0],img);
+      img.copyTo(pre_img[1],img);
+      set_flag = 0;
    } 
-    // （4）カメラから1フレーム分の画像データを取得して、変数frameに格納する
-    cap >> frame;
-    //cv::cvtColor(frame, gray, cv::COLOR_RGB2GRAY);
-    //cv::equalizeHist(gray, img1);
-    //threshold(gray, dst, thredshold_value, 200, 0);
-    // cv::GaussianBlur(gray, img2, cv::Size(7,7), 10, 10);
-    // cv::Canny(img2, dst, 40, 150);
+  //   // （4）カメラから1フレーム分の画像データを取得して、変数frameに格納する
+  //  cap >> frame;
+  //  cv::cvtColor(frame, gray, cv::COLOR_RGB2GRAY);
+  //  cv::equalizeHist(gray, img1);
+  //  cv::threshold(gray, dst, thredshold_value, 200, 0);
+  //   // cv::GaussianBlur(gray, img2, cv::Size(7,7), 10, 10);
+  //   // cv::Canny(img2, dst, 40, 150);
 
     //差分1：フレーム1と2の差を求める
     cv::absdiff(pre_img[0], pre_img[1],dif[0]);
@@ -62,12 +63,12 @@ int main(int argc, const char* argv[])
     //差分diffのうち、閾値thを超えている部分を1、それ以外を0としてmaskに出力
     cv::threshold(dif[2],gray,5,1,cv::THRESH_BINARY);
     //マスクmaskのうち、1(True)の部分を白(0)に、0(False)の部分を黒(255)にしてim_maskに出力
-    cv::threshold(gray,dist,0,255,cv::THRESH_BINARY);
+    cv::threshold(gray,dst,0,255,cv::THRESH_BINARY);
     //メディアンフィルタを使った平滑化によってゴマ塩ノイズを除去、アパーチャサイズ5
-    cv::medianBlur(dist,dist,5);
+    cv::medianBlur(dst,dst,5);
     //ウィンドウ2枚にそれぞれ入力画像、差分画像を表示
     cv::imshow("in",frame);
-    cv::imshow("out",dist);
+    cv::imshow("out",dst);
  
     //新しいフレームをカメラから一つ取り出し、3つのフレームを全てずらす
     pre_img[1].copyTo(pre_img[0],pre_img[1]);
@@ -79,7 +80,7 @@ int main(int argc, const char* argv[])
     if (frame.empty()) break;
 
     // 取得した画像データをウィンドウ表示する
-    cv::imshow("image", dst);
+    //cv::imshow("image", dst);
 
     //ウィンドウ上でEscキーが押されたらプログラム終了
     if(cv::waitKey(27)>=0)
