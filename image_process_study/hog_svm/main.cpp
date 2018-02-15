1252d0ade928fdd1f4205cf86fd4e62c97e7a114
atmos-tic <hantai8118@gmail.com>
2018-02-15 18:08:40 +0900

    [add] calHist

--- image_process_study/hog_svm/main.cpp
+++ image_process_study/hog_svm/main.cpp
@@ -5,103 +5,136 @@
 #include <iostream>
 
 #define Nbin (9)
-#define Npixel (4)
-#define Ncell  (4)
-#define Data_Size (64)
+#define Np (4)
+#define Nc  (4)
+#define DSx (64)
+#define DSy (64)
 cv::Rect selection;
 int select_object;
 void on_mouse (int event, int x, int y, int flags, void *param);
 int main(int argc, const char* argv[])
 {
-//   // 画像データを格納するための変数を宣言する
-//   cv::Mat frame;
-//   cv::Mat dst, img_diff[2], dif[3], img, clone;
+  // 画像データを格納するための変数を宣言する
+  cv::Mat frame;
+  cv::Mat dst, img_diff[2], dif[3], img, clone;
 
-//   frame = cv::imread("seacow2.jpg");
-//     if(!frame.data)
-//     return -1;
-//   cv::cvtColor(frame, img, cv::COLOR_RGB2GRAY);
-//   cv::namedWindow("in", cv::WINDOW_AUTOSIZE);
-//   cv::imshow("in", img); 
-//   cvSetMouseCallback("in", (CvMouseCallback)(&on_mouse), &img);
-//   while(!select_object){
-//     if(cv::waitKey(27)>=0)//escをおされるまで　
-//     {
-//       cv::destroyAllWindows();
-//       break;
-//     }
-//   }
-//   while(select_object){
-//     if(cv::waitKey(27)>=0)//escをおされるまで　
-//     {
-//       cv::destroyAllWindows();
-//       break;
-//     }
-//   }
-//   dst = img.clone();
-//   //選択範囲の表示
-//  // cv::rectangle(img, selection, 0xFF, 5);
-//  // cv::imshow("in", img); 
-//   //選択範囲への処理
-//   cv::Mat roi(img, selection);
-//   cv::resize(roi, dst, cv::Size(Data_Size, Data_Size));
-//   dst.convertTo(dst, CV_32F, 1);
-//   cv::GaussianBlur(dst, dst, cv::Size(7,7), 10, 10);
-//   cv::Sobel(dst, img_diff[0], CV_32F, 1, 0);
-//   cv::Sobel(dst, img_diff[1], CV_32F, 0, 1);
-//   cv::Mat mag, angle; 
-//   cv::cartToPolar(img_diff[0], img_diff[1], mag, angle, true); 
-//   mag.convertTo(mag, CV_8U, 1);
-//   cv::imshow("mag", mag);
-//   // 勾配方向を[0, 180)にする
-//   cv::add(angle, cv::Scalar(180.0), angle, angle < 0.0);
-//   cv::add(angle, cv::Scalar(-360.0), angle, angle >= 360.0);
-//   cv::add(angle, cv::Scalar(-180), angle, angle >= 180);
-//   cv::add(angle, cv::Scalar(-180.0), angle, angle >= 180.0);
-//   angle.convertTo(angle, CV_8U, 1);
-//   angle /= (180/Nbin);
-//   cv::add(angle, cv::Scalar(-1), angle, angle == Nbin);//五入防止
-//   cv::imshow("angle", angle);
-
-  cv::Mat src_img;
-  
-  int camera_id = 0;
-  //cv::VideoCapture capture(cv::CAP_DSHOW + camera_id);
-  cv::VideoCapture capture(camera_id);
-
-  // （2）カメラIDが0番のカメラが正しくオープンできているかをチェックする（正しくオープンできていなければエラー終了する）
-  if (!capture.isOpened())
+  frame = cv::imread("seacow2.jpg");
+    if(!frame.data)
     return -1;
+  cv::cvtColor(frame, img, cv::COLOR_RGB2GRAY);
+  cv::namedWindow("in", cv::WINDOW_AUTOSIZE);
+  cv::imshow("in", img); 
+  cvSetMouseCallback("in", (CvMouseCallback)(&on_mouse), &img);
+  while(!select_object){
+    if(cv::waitKey(27)>=0)//escをおされるまで　
+    {
+      cv::destroyAllWindows();
+      break;
+    }
+  }
+  while(select_object){
+    if(cv::waitKey(27)>=0)//escをおされるまで　
+    {
+      cv::destroyAllWindows();
+      break;
+    }
+  }
+  dst = img.clone();
+  //選択範囲の表示
+ // cv::rectangle(img, selection, 0xFF, 5);
+ // cv::imshow("in", img); 
+  //選択範囲への処理
+  cv::Mat roi(img, selection);
+  cv::resize(roi, dst, cv::Size(DSx, DSy));
+  dst.convertTo(dst, CV_32F, 1);
+  cv::GaussianBlur(dst, dst, cv::Size(7,7), 10, 10);
+  cv::Sobel(dst, img_diff[0], CV_32F, 1, 0);
+  cv::Sobel(dst, img_diff[1], CV_32F, 0, 1);
+  cv::Mat mag, ang; 
+  cv::cartToPolar(img_diff[0], img_diff[1], mag, ang, true); 
+  mag.convertTo(mag, CV_8U, 1);
+  cv::imshow("mag", mag);
+  // 勾配方向を[0, 180)にする
+  cv::add(ang, cv::Scalar(180.0), ang, ang < 0.0);
+  cv::add(ang, cv::Scalar(-360.0), ang, ang >= 360.0);
+  cv::add(ang, cv::Scalar(-180), ang, ang >= 180);
+  cv::add(ang, cv::Scalar(-180.0), ang, ang >= 180.0);
+  ang.convertTo(ang, CV_8U, 1);
+  ang /= (180/Nbin);
+  cv::add(ang, cv::Scalar(-1), ang, ang == Nbin);//五入防止
+  cv::imshow("angle", ang);
 
-  // （3）カメラキャプチャのフレームレートを30.0に指定する
-  capture.set(cv::CAP_PROP_FPS, 30.0);
+  //積分画像作成
+  std::vector<cv::Mat> bin, integral;
+  for(int theta = 0; theta < Nbin; theta++){
+    cv::Mat tmp1, tmp2;
+    cv::bitwise_and(ang, theta, tmp1, ang==theta);
+    cv::add(tmp1, 1-theta, tmp1, tmp1==theta);
+    tmp1 = tmp1.mul(mag);
+    cv::integral(tmp1,tmp2);
 
-	//cv::VideoCapture capture;
-	capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
-	capture.open(0);
+    bin.push_back(tmp1);
+    integral.push_back(tmp2);
+  }
+  cv::Mat Mat_bin(DSx/Np, DSy/Np, CV_8UC(9)); 
+  cv::Mat Mat_integral(DSx/Np, DSy/Np, CV_8UC(9));
+  cv::merge(bin, Mat_bin);
+  cv::merge(integral, Mat_integral);
 
-	cv::HOGDescriptor hog;
-	cv::Mat mono_img;
-	hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+  // cv::Mat hist;
+  // int channels[] = {0};
+  // float range[] = {0, 180};
+  // const float* ranges[] = {range};
+  // int histSize[] = {Nbin};
+  // cv::calcHist(&ang,1,channels,cv::Mat(),hist,1,histSize,ranges,true,false);
 
-	std::vector<cv::Rect> found;
-	while(true) {
-		capture >> src_img;
+  cv::Mat hog_hist[DSx/Np-Nc+1][DSy/Np-Nc+1];
 
-		cv::cvtColor(src_img, mono_img, CV_BGR2GRAY);
-		hog.detectMultiScale(mono_img, found);
+  for(int Sbx = 0; Sbx < DSx/Np-Nc+1; Sbx++){
+    for(int Sby = 0; Sby < DSy/Np-Nc+1; Sby++){
+      hog_hist[Sbx][Sby] = cv::Mat(Nc, Nc, CV_8UC(9));//ヒストグラム初期化
+      for(int Scx = Sbx; Scx < Sbx+Nc; Scx++){
+        for(int Scy = Sby; Scy < Sby+Nc; Scy++){ 
+          cv::Mat hist_roi(Mat_bin, cv::Rect(Sbx+(Scx-Sbx)*Np, Sby+(Scy-Sby)*Np, Np, Np));
+          hog_hist[Sbx][Sby].at<cv::Mat>(Scy, Scx) = cv::sum(hist_roi);//各セルのヒストグラム作成
 
-		for(unsigned i = 0; i < found.size(); i++) {
-			cv::Rect r = found[i];
-			rectangle(src_img, r.tl(), r.br(), cv::Scalar(0,255,0), 2);
-		}
-		cv::imshow("test", src_img);
+        }
+      }
+    }
+  }
 
-		int c = cv::waitKey(1);
-		if( c == 27) break;
-	}
- // cv::waitKey(0);
+  // cv::Mat hog_descriptor;//(Data_Size-(Nc*Np)+1, Data_Size-(Nc*Np)+1);
+  // cv::Mat cell_hist[Nbin];
+  // for(int x_block_start = 0; x_block_start < Data_Size-(Nc*Np)+1; x_block_start++){
+  //   for(int y_block_start = 0; y_block_start < Data_Size-(Nc*Np)+1; y_block_start++){
+  //     /*ブロック毎の処理*/
+  //     cv::Scalar hist_sum = 0;
+  //     cv::Scalar hog_hist[Nbin];
+  //     for(int x_cell_start = x_block_start; x_cell_start < x_block_start+Nc; x_cell_start++){
+  //       for(int y_cell_start = y_block_start; y_cell_start < y_block_start+Nc; y_cell_start++){ 
+  //         /*セル毎の処理*/
+  //         //ROIでセルの切り出し
+  //         cv::Mat mag_roi(mag, cv::Rect(x_block_start+(x_cell_start-x_block_start)*Np, y_block_start+(y_cell_start-y_block_start)*Np, Np, Np));
+  //         cv::Mat angle_roi(angle, cv::Rect(x_block_start+(x_cell_start-x_block_start)*Np, y_block_start+(y_cell_start-y_block_start)*Np, Np, Np));
+  //         //ヒストグラムの作成とその和
+  //         for(int theta = 0; theta < Nbin; theta++){
+  //           cv::Mat tmp;
+  //           cv::bitwise_and(angle_roi, theta, tmp, angle_roi==theta);
+  //           cv::add(tmp, 1-theta, tmp, tmp==theta);
+  //           hog_hist[theta] = cv::sum(tmp.mul(mag_roi));
+  //           hist_sum += hog_hist[theta];
+  //         }
+  //       }
+  //     }
+  //     cv::Mat mag_block_roi(mag, cv::Rect(x_block_start, y_block_start, Nc*Np, Nc*Np));
+  //     hog_descriptor = mag_block_roi / std::sqrt(cv::norm(hist_sum)*cv::norm(hist_sum) + 1);
+  //     std::cout<<std::sqrt(cv::norm(hist_sum)*cv::norm(hist_sum) + 1) <<std::endl;
+  //     cv::imshow("hog_descriptor", hog_descriptor);
+  //   }
+  // }
+
+  // cv::imshow("mag", mag);
+  cv::waitKey(0);
 
   return 0;
 }
