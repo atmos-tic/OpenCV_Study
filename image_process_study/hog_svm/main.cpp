ccc3d535bf9890444f045c2bacac451ff856df6e
atmos-tic <hantai8118@gmail.com>
2018-02-23 16:32:04 +0900

    [add] typedef

--- image_process_study/hog_svm/main.cpp
+++ image_process_study/hog_svm/main.cpp
@@ -18,40 +18,38 @@ int main(int argc, const char* argv[])
   cv::Mat frame;
   cv::Mat imgdst, img_diff[2], dif[3], img, clone, dst;
 
-  //frame = cv::imread("seacow2.jpg");
-  frame = cv::imread("img.jpg");
+  frame = cv::imread("seacow2.jpg");
+  //frame = cv::imread("img.jpg");
   
   if(!frame.data)
     return -1;
   cv::cvtColor(frame, img, cv::COLOR_RGB2GRAY);
   cv::namedWindow("in", cv::WINDOW_AUTOSIZE);
   cv::imshow("in", img); 
-  // cvSetMouseCallback("in", (CvMouseCallback)(&on_mouse), &img);
-  // while(!select_object){
-  //   if(cv::waitKey(27)>=0)//escをおされるまで　
-  //   {
-  //     cv::destroyAllWindows();
-  //     break;
-  //   }
-  // }
-  // while(select_object){
-  //   if(cv::waitKey(27)>=0)//escをおされるまで　
-  //   {
-  //     cv::destroyAllWindows();
-  //     break;
-  //   }
-  // }
+  cvSetMouseCallback("in", (CvMouseCallback)(&on_mouse), &img);
+  while(!select_object){
+    if(cv::waitKey(27)>=0)//escをおされるまで　
+    {
+      cv::destroyAllWindows();
+      break;
+    }
+  }
+  while(select_object){
+    if(cv::waitKey(27)>=0)//escをおされるまで　
+    {
+      cv::destroyAllWindows();
+      break;
+    }
+  }
 
   //選択範囲の表示
  // cv::rectangle(img, selection, 0xFF, 5);
  // cv::imshow("in", img); 
-  //選択範囲への処理
-  // cv::Mat roi(img, selection);
-  // cv::resize(roi, roi, cv::Size(DSx, DSy));
-  // roi.convertTo(dst, CV_32F, 1);
 
-  cv::resize(frame, img, cv::Size(DSx, DSy));
-  img.convertTo(dst, CV_32F, 1);
+  //選択範囲への処理
+  cv::Mat roi(img, selection);
+  cv::resize(roi, roi, cv::Size(DSx, DSy));
+  roi.convertTo(dst, CV_32F, 1);
   cv::GaussianBlur(dst, dst, cv::Size(7,7), 10, 10);
   cv::Sobel(dst, img_diff[0], CV_32F, 1, 0);
   cv::Sobel(dst, img_diff[1], CV_32F, 0, 1);
@@ -72,47 +70,36 @@ int main(int argc, const char* argv[])
   //積分画像作成
   std::vector<cv::Mat> bin;
   for(int theta = 0; theta < Nbin; theta++){
-    cv::Mat tmp1;
-    cv::bitwise_and(ang, theta, tmp1, ang==theta);
-    cv::add(tmp1, 1-theta, tmp1, tmp1==theta);
-    tmp1 = tmp1.mul(mag);
-    bin.push_back(tmp1);
+    cv::Mat tmp;
+    cv::bitwise_and(ang, theta, tmp, ang==theta);
+    cv::add(tmp, 1-theta, tmp, tmp==theta);
+    tmp = tmp.mul(mag);
+    bin.push_back(tmp);
   }
-  cv::Mat Mat_bin;//(DSy/Np, DSx/Np, CV_8UC(Nbin)); 
+  cv::Mat Mat_bin(DSy/Np, DSx/Np, CV_8UC(Nbin)); 
   cv::merge(bin, Mat_bin);
+
+  typedef cv::Matx<double, 9, 1> Matx9d;
 
   cv::Mat hog_hist[DSx/Np-Nc+1][DSy/Np-Nc+1];
   for(int Sbx = 0; Sbx < DSx/Np-Nc+1; Sbx++){
     for(int Sby = 0; Sby < DSy/Np-Nc+1; Sby++){
       //各ブロック内の処理
-      hog_hist[Sbx][Sby] = cv::Mat(Nc, Nc, CV_8UC(Nbin));//ヒストグラム初期化
+      hog_hist[Sbx][Sby] = cv::Mat::zeros(Nc, Nc, CV_8UC(Nbin));//ヒストグラム初期化
       for(int Scx = 0; Scx < Nc; Scx++){
         for(int Scy = 0; Scy < Nc; Scy++){ 
           //各セル内の処理
-          //ここが悪い
           cv::Mat hist_roi = Mat_bin(cv::Rect(Sby*Np+Scy*Np, Sbx*Np+Scx*Np, Np, Np));//全体からセルのデータ抽出
           cv::Mat tmp;
           cv::integral(hist_roi,tmp);
-          //std::cout<<hist_roi<<std::endl;
-          hog_hist[Sbx][Sby].at<unsigned char>(Scy, Scx) = tmp.at<unsigned char>(Np, Np);//各セルのヒストグラム作成
+          //std::cout<<tmp.at<Matx9d>(Np, Np)<<std::endl;
+          hog_hist[Sbx][Sby].at<Matx9d>(Scy, Scx) = tmp.at<Matx9d>(Np, Np);//各セルのヒストグラム作成
         }
       }
-
-      // std::vector<cv::Mat> tmp;
-      // cv::Mat sum = cv::Mat::zeros(Nc,Nc,CV_8UC1);
-      // cv::split(hog_hist[Sbx][Sby], tmp);
-      // for(int theta = 0; theta < Nbin; theta++){
-      //   sum += tmp[theta];
-      // }
-      // sum = cv::sqrt(norm(sum)*norm(sum) + 1);
-      // for(int theta = 0; theta < Nbin; theta++){
-      //   tmp[theta] /= sum;
-      // }
-      // cv::merge(tmp, hog_hist[Sbx][Sby]);
       
       double sum;
       sum = cv::sqrt(norm(hog_hist[Sbx][Sby])*norm(hog_hist[Sbx][Sby]) + 1);
-      std::cout<<sum<<"x"<<Sbx<<"y"<<Sby<<std::endl;
+      std::cout<<norm(hog_hist[Sbx][Sby])<<"x"<<Sbx<<"y"<<Sby<<std::endl;
       // //std::cout<<hog_hist[Sbx][Sby]<<std::endl;
       hog_hist[Sbx][Sby] /= sum;
       cv::Mat tmp(Nc, Nc, CV_8UC(9));
@@ -125,12 +112,11 @@ int main(int argc, const char* argv[])
         cv::Point rd(Np*0.5*cos(theta), Np*0.5*sin(theta));
         cv::Point rp = center -   rd;
         cv::Point lp = center -  -rd;
-        // cv::line(roi, rp, lp, cv::Scalar(255*tmp.at<cv::Scalar>(Nc, Nc)[i], 255, 255));
-        cv::line(img, rp, lp, cv::Scalar(255*tmp.at<cv::Scalar>(Nc, Nc)[i], 255, 255)); 
+        cv::line(roi, rp, lp, cv::Scalar(255*tmp.at<cv::Scalar>(Nc, Nc)[i], 255, 255)); 
       }     
     }
   }
-  cv::imshow("img", img);
+  cv::imshow("img", roi);
   cv::waitKey(0);
 
   return 0;
