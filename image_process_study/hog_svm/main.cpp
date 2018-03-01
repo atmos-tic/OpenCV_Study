508ee1de449e632eea3602a0c704ff9fbd370bcc
atmos-tic <hantai8118@gmail.com>
2018-03-01 16:59:15 +0900

    [update] dynamics image

--- image_process_study/hog_svm/main.cpp
+++ image_process_study/hog_svm/main.cpp
@@ -18,117 +18,121 @@ int main(int argc, const char* argv[])
   cv::Mat frame;
   cv::Mat imgdst, img_diff[2], dif[3], img, clone, dst;
 
-  img = cv::imread("l_hires.jpg");
-  //frame = cv::imread("img.jpg");
-  
-  if(!img.data)
+// （1）DirectShowを使ってカメラキャプチャを行う。
+  // また、カメラIDが0番のカメラについてキャプチャの準備を行う
+  int camera_id = 0;
+  //cv::VideoCapture cap(cv::CAP_DSHOW + camera_id);
+   cv::VideoCapture cap(camera_id);
+
+  // （2）カメラIDが0番のカメラが正しくオープンできているかをチェックする（正しくオープンできていなければエラー終了する）
+  if (!cap.isOpened())
     return -1;
-  cv::cvtColor(img, img, cv::COLOR_RGB2GRAY);
-  cv::namedWindow("in", cv::WINDOW_AUTOSIZE);
-  cv::imshow("in", img); 
-  cvSetMouseCallback("in", (CvMouseCallback)(&on_mouse), &img);
-  while(!select_object){
-    if(cv::waitKey(27)>=0)//escをおされるまで　
-    {
-      cv::destroyAllWindows();
-      break;
-    }
-  }
-  while(select_object){
-    if(cv::waitKey(27)>=0)//escをおされるまで　
-    {
-      cv::destroyAllWindows();
-      break;
-    }
-  }
 
-  //選択範囲の表示
- // cv::rectangle(img, selection, 0xFF, 5);
- // cv::imshow("in", img); 
+  // （3）カメラキャプチャのフレームレートを30.0に指定する
+  cap.set(cv::CAP_PROP_FPS, 30.0);
 
-  //選択範囲への処理
-  cv::Mat roi(img, selection);
-  cv::resize(roi, roi, cv::Size(DSx, DSy));
-  roi.convertTo(dst, CV_32F, 1);
-  cv::GaussianBlur(dst, dst, cv::Size(7,7), 10, 10);
-  cv::Sobel(dst, img_diff[0], CV_64F, 1, 0);
-  cv::Sobel(dst, img_diff[1], CV_64F, 0, 1);
-  cv::Mat mag, ang; 
-  cv::cartToPolar(img_diff[0], img_diff[1], mag, ang, true); 
-  //mag.convertTo(mag, CV_8U, 1);
-  //cv::imshow("mag", mag);
-  // 勾配方向を[0, 180)にする
-  cv::add(ang, cv::Scalar(180.0), ang, ang < 0.0);
-  cv::add(ang, cv::Scalar(-180), ang, ang >= 180);
-  ang.convertTo(ang, CV_8U, 1);
-  ang /= (180/Nbin);
-  ang.convertTo(ang, CV_8U, 1);
-  cv::add(ang, cv::Scalar(-1), ang, ang == 9);
-  ang.convertTo(ang, CV_64FC1, 1);
-  //cv::imshow("ang", ang);     
 
-  //積分画像作成
-  std::vector<cv::Mat> bin;
-  cv::add(ang, 1, ang);
-  for(int theta = 0; theta < Nbin; theta++){
-    cv::Mat tmp;
-    cv::bitwise_and(ang, theta+1, tmp, ang==theta+1);
-    cv::add(tmp, -theta, tmp, tmp==theta+1);
-    tmp = tmp.mul(mag);
-    bin.push_back(tmp);
-  }
-  cv::Mat Mat_bin(DSy/Np, DSx/Np, CV_64FC(Nbin)); 
-  cv::merge(bin, Mat_bin);
+  while(true) {
+		cap >> img;
 
-  typedef cv::Vec<double, 9> Vec9d;
+    //img = cv::imread("l_hires.jpg");
+    //frame = cv::imread("img.jpg");
+    
+    if(!img.data)
+      return -1;
+    cv::cvtColor(img, img, cv::COLOR_RGB2GRAY);
+    //cv::namedWindow("in", cv::WINDOW_AUTOSIZE);
+    // cv::imshow("in", img); 
+    // cvSetMouseCallback("in", (CvMouseCallback)(&on_mouse), &img);
+    // while(!select_object){
+    //   if(cv::waitKey(27)>=0)//escをおされるまで　
+    //   {
+    //     cv::destroyAllWindows();
+    //     break;
+    //   }
+    // }
+    // while(select_object){
+    //   if(cv::waitKey(27)>=0)//escをおされるまで　
+    //   {
+    //     cv::destroyAllWindows();
+    //     break;
+    //   }
+    // }
 
-  cv::Mat hog_hist[DSx/Np-Nc+1][DSy/Np-Nc+1];
-  for(int Sbx = 0; Sbx < DSx/Np-Nc+1; Sbx++){
-    for(int Sby = 0; Sby < DSy/Np-Nc+1; Sby++){
-      //各ブロック内の処理
-      hog_hist[Sbx][Sby] = cv::Mat(Nc, Nc, CV_64FC(Nbin));//ヒストグラム初期化
-      for(int Scx = 0; Scx < Nc; Scx++){
-        for(int Scy = 0; Scy < Nc; Scy++){ 
-          //各セル内の処理
-          cv::Mat hist_roi = Mat_bin(cv::Rect(Sby*Np+Scy*Np, Sbx*Np+Scx*Np, Np, Np));//全体からセルのデータ抽出
-          cv::Mat tmp;
-          cv::integral(hist_roi,tmp);         
-          for(int theta = 0; theta < Nbin; theta++){
-            //std::cout<<tmp.at<Vec9f>(Np, Np)[theta]<<std::endl;      
-            //std::cout<<tmp.at<double>(Np, Np*Nbin + theta)<<std::endl;      
-            hog_hist[Sbx][Sby].at<double>(Scy, Scx*Nbin + theta) = tmp.at<double>(Np, Np*Nbin + theta);//各セルのヒストグラム作成
-            //hog_hist[Sbx][Sby].at<Vec9d>(Scy, Scx)[theta] = tmp.at<Vec9d>(Np, Np)[theta];//各セルのヒストグラム作成
-          }    
-        }
-      }
-      
-      double sum;
-      sum = cv::sqrt(norm(hog_hist[Sbx][Sby])*norm(hog_hist[Sbx][Sby]) + 1);
-      //std::cout<<hog_hist[Sbx][Sby]<<std::endl;
-      //std::cout<<sum<<"|"<<norm(hog_hist[Sbx][Sby])*norm(hog_hist[Sbx][Sby])<<"|"<<norm(hog_hist[Sbx][Sby])<<"x"<<Sbx<<"y"<<Sby<<std::endl;
-      hog_hist[Sbx][Sby] /= sum;
-     // std::cout<<hog_hist[Sbx][Sby]<<std::endl;
+    //選択範囲への処理
+    //cv::Mat roi(img, selection);
+    cv::Mat roi;
+    cv::resize(img, roi, cv::Size(DSx, DSy));
+    roi.convertTo(dst, CV_32F, 1);
+    cv::GaussianBlur(dst, dst, cv::Size(7,7), 10, 10);
+    cv::Sobel(dst, img_diff[0], CV_64F, 1, 0);
+    cv::Sobel(dst, img_diff[1], CV_64F, 0, 1);
+    cv::Mat mag, ang; 
+    cv::cartToPolar(img_diff[0], img_diff[1], mag, ang, true); 
+    // 勾配方向を[0, 180)にする
+    cv::add(ang, cv::Scalar(180.0), ang, ang < 0.0);
+    cv::add(ang, cv::Scalar(-180), ang, ang >= 180);
+    ang.convertTo(ang, CV_8U, 1);
+    ang /= (180/Nbin);
+    ang.convertTo(ang, CV_8U, 1);
+    cv::add(ang, cv::Scalar(-1), ang, ang == 9);
+    ang.convertTo(ang, CV_64FC1, 1);
+    //cv::imshow("ang", ang);    
+
+    //積分画像作成
+    std::vector<cv::Mat> bin;
+    cv::add(ang, 1, ang);
+    for(int theta = 0; theta < Nbin; theta++){
       cv::Mat tmp;
-      cv::integral(hog_hist[Sbx][Sby],tmp);
-     // std::cout<<tmp<<std::endl;
-
-      //角度ごとに線を描画
-      cv::Point center(Sbx*Np+(Nc*Np)/2,Sby*Np+(Nc*Np)/2);
-      for (int i = 0; i < Nbin; i++) {
-        double theta = (i * 180 / Nbin + 90) * CV_PI / 180.0;
-        cv::Point rd(Np*0.5*cos(theta), Np*0.5*sin(theta));
-        cv::Point rp = center + rd;
-        cv::Point lp = center - rd;
-        //cv::line(roi, rp, lp, cv::Scalar(255*tmp.at<Vec9d>(Nc, Nc*Nbin)[i], 255, 255)); 
-        cv::line(roi, rp, lp, cv::Scalar(255*tmp.at<double>(Nc, Nc*Nbin + i), 255, 255)); 
-        //std::cout<<hog_hist[Sbx][Sby].at<double>(Nc-1, (Nc-1)*Nbin+i)<<" "<<theta*180/CV_PI<<std::endl;
-        //std::cout<<theta*180/CV_PI<<" "<<tmp.at<double>(Nc, Nc*Nbin+i)<<std::endl;
-      }     
+      cv::bitwise_and(ang, theta+1, tmp, ang==theta+1);
+      cv::add(tmp, -theta, tmp, tmp==theta+1);
+      tmp = tmp.mul(mag);
+      std::cout<<cv::sum(tmp)<<"|"<<cv::norm(tmp)<<std::endl;      
+      bin.push_back(tmp);
     }
-  }
-  cv::imshow("img", roi);
-  cv::waitKey(0);
+    cv::Mat Mat_bin(DSy/Np, DSx/Np, CV_64FC(Nbin)); 
+    cv::merge(bin, Mat_bin);
 
+    typedef cv::Vec<double, 9> Vec9d;
+
+    cv::Mat hog_hist[DSy/Np-Nc+1][DSx/Np-Nc+1];
+    for(int Sbx = 0; Sbx < DSx/Np-Nc+1; Sbx++){
+      for(int Sby = 0; Sby < DSy/Np-Nc+1; Sby++){
+        //各ブロック内の処理
+        hog_hist[Sby][Sbx] = cv::Mat(Nc, Nc, CV_64FC(Nbin));//ヒストグラム初期化
+        for(int Scx = 0; Scx < Nc; Scx++){
+          for(int Scy = 0; Scy < Nc; Scy++){ 
+            //各セル内の処理
+            cv::Mat hist_roi = Mat_bin(cv::Rect(Sby*Np+Scy*Np, Sbx*Np+Scx*Np, Np, Np));//全体からセルのデータ抽出
+            cv::Mat tmp;
+            cv::integral(hist_roi,tmp);         
+            for(int theta = 0; theta < Nbin; theta++){    
+              hog_hist[Sby][Sbx].at<double>(Scy, Scx*Nbin + theta) = tmp.at<double>(Np, Np*Nbin + theta);//各セルのヒストグラム作成
+            }    
+          }
+        }
+        
+        double sum;
+        sum = cv::sqrt(norm(hog_hist[Sby][Sbx])*norm(hog_hist[Sby][Sbx]) + 1);
+        hog_hist[Sby][Sbx] /= sum;
+        cv::Mat tmp;
+        cv::integral(hog_hist[Sby][Sbx],tmp);
+
+        //角度ごとに線を描画
+        cv::Point center(Sby*Np+(Nc*Np)/2, Sbx*Np+(Nc*Np)/2);
+        for (int i = 0; i < Nbin; i++) {
+          double theta = (i * 180 / Nbin + 90) * CV_PI / 180.0;
+          cv::Point rd(Np*0.5*cos(theta), Np*0.5*sin(theta));
+          cv::Point rp = center + rd;
+          cv::Point lp = center - rd;
+          cv::line(roi, rp, lp, cv::Scalar(255*tmp.at<double>(Nc, Nc*Nbin + i), 255*tmp.at<double>(Nc, Nc*Nbin + i), 255*tmp.at<double>(Nc, Nc*Nbin + i))); 
+        }     
+      }
+    }
+    cv::imshow("img", roi);
+    int c = cv::waitKey(1);
+		if( c == 27) break;
+  }
   return 0;
 }
 
